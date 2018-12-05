cf81a8aa5a7c30be760f8de3d8d06443fb0d26fa
rahulsantha2006 <rahul.santha.2006@gmail.com>
2018-12-04 20:23:37 -0600

    Added script file and some functions

---
+++ script1.js
@@ -0,0 +1,8 @@
+function buttonChanger() {
+    document.getElementById("demo2").innerHTML = "rahul is the super best";
+}
+
+function showAlert() {
+    document.getElementById("demo2").innerHTML = "do you see a alert";
+    alert("this is going to hack you");
+}
---
+++ script2.js
@@ -0,0 +1,16 @@
+
+function contentChange() {
+    document.getElementById('demo').innerHTML= 'This has changed because of javascript';
+}
+
+function styleChange() {
+    document.getElementById('demo').style.fontSize='35px';
+}
+
+function hidePara() {
+    document.getElementById('p2').style.display="none";
+}
+
+function showPara() {
+    document.getElementById('p2').style.display="block";
+}
